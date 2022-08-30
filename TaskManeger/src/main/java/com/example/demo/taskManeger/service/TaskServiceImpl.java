5d5e9c13270112748349d0f560d84ff81f6a9bfb
shouta108 <221054@st.yoshida-g.ac.jp>
2022-08-30 13:59:39 +0900

    css追加

--- TaskManeger/src/main/java/com/example/demo/taskManeger/service/TaskServiceImpl.java
+++ TaskManeger/src/main/java/com/example/demo/taskManeger/service/TaskServiceImpl.java
@@ -3,7 +3,6 @@ package com.example.demo.taskManeger.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.config.Task;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
